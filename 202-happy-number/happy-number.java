c791f8d1a880c15809e91fd3061f1dfe5ac8e88c
xNeeyonch <168049566+xNeeyonch@users.noreply.github.com>
2025-05-23 18:47:37 -0700

    Time: 1 ms (67.58%) | Memory: 40.9 MB (53.08%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        /*
+        - separate n into its digits
+        - raise them to the power of 2 then add them
+        - RETURN TRUE IF the sum of the numbers is 1
+        - if not then continue the cycle until it reaches 1
+        - sum cannot be repeating ( store in list to check list if it exists)
+        - if found current sum in list, return false
+        */
+        
+        //first create the list
+        Set<Integer> listOfSum = new HashSet<>();
+        
+        //create a while infinite while loop so that it 
+        //returns either true or false
+        while (true){
+
+            int sum = 0;
+
+            //a while loop to seperate the digits of n and 
+            //square them then add them to the sum
+            while (n != 0) {
+                sum += Math.pow(n % 10, 2);
+                n /= 10;
+            }
+
+            
+            if (sum == 1){
+                return true;
+            }
+
+            //make n equal the new sum 
+            n = sum;
+
+            if (listOfSum.contains(n)){
+                return false;
+            }
+            listOfSum.add(n);
+
+        }
+    }
+    
+}
