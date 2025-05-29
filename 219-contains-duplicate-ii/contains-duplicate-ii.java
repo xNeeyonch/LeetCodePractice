106fac559db924df0d99f9ce07834a2224da4ef1
xNeeyonch <168049566+xNeeyonch@users.noreply.github.com>
2025-05-28 21:02:57 -0700

    Time: 20 ms (91.57%) | Memory: 60.5 MB (14.70%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -2,20 +2,34 @@ class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
 
         /*
-        NOTES 
+        NOTES
         - check if array has a duplicate of a number
-        - the number of elements in between the two duplicates should be less 
-            than or equal to k 
-            
+        - the number of elements in between the two duplicates should be less
+            than or equal to k
+
         APPROACH
-        - make 2 fori loops
-        - increment the forj loop until j <= k
+        - insert all the elements in nums array into a hashmap where
+        - the key will equal the element value and the value will be the index will be value
+        - create a fori loop to insert each index into the hashmap
+        - if the hashmap already contains the same key(value) then check if the value
+            is greater than or equal to k
+        - if this is true then return true
+        - if false then replace the value(index) with the new index and repeat
+        - if no solutions found then return false
          */
 
+        HashMap<Integer, Integer> newNums = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j <= i + k && j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
+            if (!newNums.containsKey(nums[i])){
+                newNums.put(nums[i], i);
+            } else {
+                
+                int difference = Math.abs(newNums.get(nums[i]) - i);
+                if (difference <= k){
                     return true;
+                } else {
+                    newNums.put(nums[i], i);
                 }
             }
         }
