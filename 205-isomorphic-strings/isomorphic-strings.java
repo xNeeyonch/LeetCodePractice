6c32af846b90df6adf8fd4d287f8315a4d0824dc
xNeeyonch <168049566+xNeeyonch@users.noreply.github.com>
2025-06-18 16:48:32 -0700

    Time: 11 ms (67.38%) | Memory: 42.2 MB (97.39%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
         HashMap<Character, Character> map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
